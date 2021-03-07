2d0f18fe1dc27606b904fa7adceecf59f31b6f7b
enessengul97 <77448460+enessengul97@users.noreply.github.com>
2021-03-07 05:39:07 +0300

    FinalProject
    Entity framework and linq tests in Northwind database

---
+++ ConsoleUI/Program.cs
@@ -0,0 +1,26 @@
+﻿using Business.Concrete;
+using DataAccess.Concrete.EntityFramework;
+using DataAccess.Concrete.InMemory;
+using Entities.Concrete;
+using System;
+
+namespace ConsoleUI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ProductManager productManager = new ProductManager( new EfProductDal());
+
+            foreach (var item in productManager.GetAllByUnitPrice(40,100))
+            {
+                Console.WriteLine(item.ProductName);
+            }
+            
+            
+
+
+
+        }
+    }
+}
---
+++ DataAccess/Abstract/IEntityRepository.cs
@@ -0,0 +1,24 @@
+﻿using Entities.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Text;
+
+namespace DataAccess.Abstract
+{
+    /*
+    generic constraint
+    class : referance type
+    IEntity :  IEntity or IEntity is implement in class
+    new() : IEntity NOT new()
+    */
+
+    public interface IEntityRepository <T> where T:class,IEntity,new()
+    {
+        List<T> GetAll(Expression<Func<T,bool>> filter = null);
+        T Get(Expression<Func<T, bool>> filter);
+        void Add(T entity);
+        void Update(T entity);
+        void Delete(T entity);
+    }
+}
